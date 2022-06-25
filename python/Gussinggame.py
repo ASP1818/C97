677b734a1dbd98db596a1a8b390e0a955bd8e6cb
Amruta Satish Pawar <89676876+ASP1818@users.noreply.github.com>
2022-06-25 17:42:35 +0530

    Add files via upload

---
+++ python/Gussinggame.py
@@ -0,0 +1,11 @@
+age = input("Enter your age: ")
+age[1, 2, 3, 4, 5, 6]
+
+while change < 5:
+
+    if guess == numbers: 
+        print("congratualation you win!")
+        break
+
+    if not changes < 5: 
+        print("you lose!! The number is", number)
